6274bf9a47a00450dc5186e452456c65169fd588
Ulf Norell <ulf.norell@gmail.com>
2015-03-23 12:56:16 +0100

    [component] Added call/2,3 for ?APPLY/2,3

--- lib/component.ex
+++ lib/component.ex
@@ -1,49 +1,62 @@
 defmodule EQC.Component do
-  @copyright "Quviq AB, 2014-2015"
+@copyright "Quviq AB, 2014-2015"
 
-  @moduledoc """
-  This module contains macros to be used with [Quviq
-  QuickCheck](http://www.quviq.com). It defines Elixir versions of the Erlang
-  macros found in `eqc/include/eqc_component.hrl`. For detailed documentation of the
-  macros, please refer to the QuickCheck documentation.
+@moduledoc """
+This module contains macros to be used with [Quviq
+QuickCheck](http://www.quviq.com). It defines Elixir versions of the Erlang
+macros found in `eqc/include/eqc_component.hrl`. For detailed documentation of the
+macros, please refer to the QuickCheck documentation.
 
-  `Copyright (C) Quviq AB, 2014-2015.`
+`Copyright (C) Quviq AB, 2014-2015.`
 """
 
-  defmacro __using__(_opts) do
+defmacro __using__(_opts) do
+  quote do
+    import :eqc_component, only: [commands: 1, commands: 2]
+    import :eqc_statem, only: [eq: 2, command_names: 1]
+    import EQC.Component
+
+    @file "eqc_component.hrl"
+    @compile {:parse_transform, :eqc_group_commands}
+    @compile {:parse_transform, :eqc_transform_callouts}
+  end
+end
+
+# -- Callout language -------------------------------------------------------
+
+def call(mod, fun, args), do: {:self_callout, mod, fun, args}
+defmacro call(fun, args) do
+  mod = Macro.var :mod, __MODULE__
+  quote do
+    call(mod, fun, args)
+  end
+end
+
+# -- Wrapper functions ------------------------------------------------------
+
+def run_commands(mod, cmds) do
+  run_commands(mod, cmds, []) end
+
+def run_commands(mod, cmds, env) do
+  {history, state, result} = :eqc_component.run_commands(mod, cmds, env)
+  [history: history, state: state, result: result]
+end
+
+def pretty_commands(mod, cmds, res, bool) do
+  :eqc_component.pretty_commands(mod, cmds,
+                              {res[:history], res[:state], res[:result]},
+                              bool)
+end
+
+defmacro weight(state, cmds) do
+  for {cmd, w} <- cmds do
     quote do
-      import :eqc_component, only: [commands: 1, commands: 2]
-      import :eqc_statem, only: [eq: 2, command_names: 1]
-      import EQC.Component
-
-      @file "eqc_component.hrl"
-      @compile {:parse_transform, :eqc_group_commands}
+      def weight(unquote(state), unquote(cmd)) do unquote(w) end
     end
-  end
-
-  def run_commands(mod, cmds) do
-    run_commands(mod, cmds, []) end
-
-  def run_commands(mod, cmds, env) do
-    {history, state, result} = :eqc_component.run_commands(mod, cmds, env)
-    [history: history, state: state, result: result]
-  end
-
-  def pretty_commands(mod, cmds, res, bool) do
-    :eqc_component.pretty_commands(mod, cmds,
-                                {res[:history], res[:state], res[:result]},
-                                bool)
-  end
-
-  defmacro weight(state, cmds) do
-    for {cmd, w} <- cmds do
-      quote do
-        def weight(unquote(state), unquote(cmd)) do unquote(w) end
-      end
-    end ++
-      [ quote do
-          def weight(unquote(state), _) do 1 end
-        end ]
-  end
+  end ++
+    [ quote do
+        def weight(unquote(state), _) do 1 end
+      end ]
+end
 
 end
