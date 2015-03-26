cb63fdb0f3f823447f61bf34833845029b6b94b8
Ulf Norell <ulf.norell@gmail.com>
2015-03-26 14:55:15 +0100

    [component] added documentation

--- lib/component.ex
+++ lib/component.ex
@@ -24,12 +24,35 @@ end
 
 # -- Callout language -------------------------------------------------------
 
+@doc """
+Call a command from a callout.
+
+In Erlang: `?APPLY(Mod, Fun, Args)`.
+"""
 def call(mod, fun, args), do: {:self_callout, mod, fun, args}
+
+@doc """
+Call a local command from a callout.
+
+In Erlang: `?APPLY(Fun, Args)`.
+"""
 defmacro call(fun, args) do
   quote do
     call(__MODULE__, unquote(fun), unquote(args))
   end
 end
+
+@doc """
+Convenient syntax for `call`.
+
+    call m.f(e1, .., en)
+    call f(e1, .., en)
+
+is equivalent to
+
+    call(m, f, [e1, .., en])
+    call(f, [e1, .., en])
+"""
 defmacro call({{:., _, [mod, fun]}, _, args}) do
   quote do call(unquote(mod), unquote(fun), unquote(args)) end
 end
@@ -38,8 +61,22 @@ defmacro call({fun, _, args}) when is_atom(fun) do
 end
 defmacro call(_), do: raise(ArgumentError, "Usage: call F(E1, .., En)")
 
+@doc """
+Specify a callout.
+
+In Erlang: `?CALLOUT(Mod, Fun, Args, Res)`.
+"""
 def callout(mod, fun, args, res), do: :eqc_component.callout(mod, fun, args, res)
 
+@doc """
+Convenient syntax for `callout`.
+
+    callout m.f(e1, .., en), return: res
+
+is equivalent to
+
+    callout(m, f, [e1, .., en], res)
+"""
 defmacro callout({{:., _, [mod, fun]}, _, args}, [return: res]) do
   quote do callout(unquote(mod), unquote(fun), unquote(args), unquote(res)) end
 end
@@ -52,11 +89,22 @@ defp do_match_gen(e) do
   quote do {:"$eqc_callout_match_gen", unquote(e)} end
 end
 
+@doc """
+Bind the result of a callout or generator.
+
+Usage:
+
+    match pat = exp
+    match pat <- gen
+
+In Erlang: `?MATCH(Pat, Exp)` or `?MATCH_GEN(Pat, Gen)`.
+"""
 defmacro match(e={:=,  _, [_, _]}), do: do_match(e)
 defmacro match(e={:<-, _, [_, _]}), do: do_match_gen(e)
 defmacro match(_), do: raise(ArgumentError, "Usage: match PAT = EXP, or match PAT <- GEN")
 
 # Hacky. Let's you write (for instance) match pat = case exp do ... end.
+@doc false
 defmacro match({:=, cxt1, [pat, {fun, cxt2, args}]}, opts) do
   do_match({:=, cxt1, [pat, {fun, cxt2, args ++ [opts]}]})
 end
@@ -64,28 +112,61 @@ defmacro match({:<-, cxt1, [pat, {fun, cxt2, args}]}, opts) do
   do_match({:<-, cxt1, [pat, {fun, cxt2, args ++ [opts]}]})
 end
 
+@doc """
+Model failure.
+
+In Erlang: `?FAIL(E)`.
+"""
 def fail(e), do: {:fail, e}
 
+@doc """
+Exception return value. Can be used as the return value for a callout to make it throw an exception.
+
+In Erlang: `?EXCEPTION(e)`.
+"""
 defmacro exception(e) do
   quote do {:"$eqc_exception", unquote(e)} end
 end
 
 # -- Wrapper functions ------------------------------------------------------
 
+@doc """
+Same as `:eqc_component.run_commands/2` but returns a keyword list with
+`:history`, `:state`, and `:result` instead of a tuple.
+"""
 def run_commands(mod, cmds) do
   run_commands(mod, cmds, []) end
 
+@doc """
+Same as `:eqc_component.run_commands/3` but returns a keyword list with
+`:history`, `:state`, and `:result` instead of a tuple.
+"""
 def run_commands(mod, cmds, env) do
   {history, state, result} = :eqc_component.run_commands(mod, cmds, env)
   [history: history, state: state, result: result]
 end
 
+@doc """
+Same as `:eqc_component.pretty_commands/4` but takes a keyword list with
+`:history`, `:state`, and `:result` instead of a tuple as the third argument.
+"""
 def pretty_commands(mod, cmds, res, bool) do
   :eqc_component.pretty_commands(mod, cmds,
                               {res[:history], res[:state], res[:result]},
                               bool)
 end
 
+@doc """
+Generate a weight function given a keyword list of command names and weights.
+
+Usage:
+
+    weight state,
+      cmd1: weight1,
+      cmd2: weight2
+
+Commands not in the list get weight 1.
+"""
 defmacro weight(state, cmds) do
   for {cmd, w} <- cmds do
     quote do
