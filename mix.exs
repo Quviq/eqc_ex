dd773c8eac0ca8a13cb9afad91dc9588c9d787e0
Alexei Sholik <alcosholik@gmail.com>
2015-05-19 12:24:24 +0300

    Clean up the mess in mix.exs and config.exs

--- mix.exs
+++ mix.exs
@@ -1,38 +1,41 @@
 defmodule EQC.Mixfile do
   use Mix.Project
 
+  @version "1.2.0"
+
   def project do
-    [ app: :eqc_ex,
-      version: "1.2.0",
+    [
+      app: :eqc_ex,
+      version: @version,
       elixir: "~> 1.0",
       deps: deps,
-      package: [ contributors: ["Quviq AB"],
-                 licenses: ["BSD"],
-                 files: ["lib", "mix.exs", "LICENSE", "README.md"],
-                 links: %{"quviq.com" => "http://www.quviq.com"}
-               ],
-      docs: [readme: "README.md", main: "EQC"],
-      description: "Wrappers to facilitate using Quviq QuickCheck with Elixir."
+      docs: docs,
+      package: package,
+      description: "Wrappers to facilitate using Quviq QuickCheck with Elixir.",
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
-    [] 
+    []
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
+  defp docs do
+    [
+      readme: "README.md",
+      main: "EQC",
+    ]
+  end
+
+  defp package do
+    [
+      contributors: ["Quviq AB"],
+      licenses: ["BSD"],
+      files: ["lib", "mix.exs", "LICENSE", "README.md"],
+      links: %{"quviq.com" => "http://www.quviq.com"}
+    ]
+  end
+
   defp deps do
-    [{:ex_doc, github: "elixir-lang/ex_doc", only: :dev}]
+    [{:ex_doc, github: "elixir-lang/ex_doc", only: :docs}]
   end
 end
