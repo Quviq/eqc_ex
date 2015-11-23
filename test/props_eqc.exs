288d6cf3ce938af77ec41a365047d59290ef7981
Thomas Arts <thomas.arts@quviq.com>
2015-11-23 11:01:34 +0100

    Some test examples

--- test/props_eqc.exs
+++ test/props_eqc.exs
@@ -2,6 +2,8 @@ defmodule SimpleTests do
   use ExUnit.Case
   use EQC.ExUnit
 
+  #@moduletag numtests: 80
+  
   property "naturals are >= 0" do
     :eqc.fails(
     forall n <- nat do
@@ -9,6 +11,7 @@ defmodule SimpleTests do
     end)
   end
 
+  @tag numtests: 31
   property "implies fine" do
       forall {n,m} <- {int, nat} do
         implies m > n, do:
@@ -16,4 +19,19 @@ defmodule SimpleTests do
     end
   end
 
+  @tag min_time: 2000 
+  property "min testing_time" do
+    forall _min <- int do
+      true
+    end
+  end
+  
+  @tag timeout: 5000
+  @tag min_time: 4000 
+  property "min testing_time too long" do
+    forall _min_long <- int do
+      true
+    end
+  end
+  
 end
