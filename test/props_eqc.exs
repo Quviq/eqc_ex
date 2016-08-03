9b48065c87f3075a76ed01295cfa6f2e728dd9aa
Thomas Arts <thomas.arts@quviq.com>
2016-08-03 15:09:58 +0200

    Remove warnings: generators are functions

--- test/props_eqc.exs
+++ test/props_eqc.exs
@@ -8,14 +8,14 @@ defmodule SimpleTests do
   @tag erlang_counterexample: false
   @tag zero: 0
   property "naturals are >= 0", context do
-    forall n <- nat do
+    forall n <- nat() do
       ensure n > context.zero
     end
   end
 
   @tag numtests: 31
   property "implies fine" do
-      forall {n,m} <- {int, nat} do
+      forall {n,m} <- {int(), nat()} do
         implies m > n, do:
         ensure m > n
     end
@@ -23,7 +23,7 @@ defmodule SimpleTests do
 
   @tag min_time: 2000 
   property "min testing_time" do
-    forall _min <- int do
+    forall _min <- int() do
       true
     end
   end
@@ -31,15 +31,15 @@ defmodule SimpleTests do
   @tag timeout: 5000
   @tag min_time: 4000 
   property "min testing_time too long" do
-    forall _min_long <- int do
+    forall _min_long <- int() do
       true
     end
   end
 
-  def string, do: utf8
+  def string(), do: utf8()
   
   property "reverse strings", context do
-    forall s <- string do
+    forall s <- string() do
       ensure String.reverse(String.reverse(s)) == s
     end
   end
