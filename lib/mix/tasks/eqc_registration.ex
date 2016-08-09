bf6fe13a991d0928688bf4910b4cad171a97af23
Thomas Arts <thomas.arts@quviq.com>
2016-08-09 14:17:29 +0200

    Add mix task for registration quickcheck licence

---
+++ lib/mix/tasks/eqc_registration.ex
@@ -0,0 +1,37 @@
+defmodule Mix.Tasks.Eqc.Registration do
+  use Mix.Task
+
+  @shortdoc "Register Quviq QuickCheck full version"
+
+  @moduledoc """
+  A Mix task for registration of QuickCheck when registration key has been provided. Only needed for commercial version.
+
+  ONLY register your QuickCheck licence once. After first registration you should install and uninstall versions without using registration again. 
+
+  ## Examples
+
+      mix eqc.register MyRegistrationID
+
+  """
+
+  @spec run(OptionParser.argv) :: boolean
+  def run(argv) do
+    {_opts, args, _} = OptionParser.parse(argv)
+    case args do
+      [key|_] ->
+        if :code.which(:eqc) == :non_existing do
+          Mix.raise """
+            Error: QuickCheck not found
+            Use mix eqc.install to install QuickCheck
+          """
+        else
+          unless :eqc.start() == :ok, do: :eqc.registration(key)
+        end
+      [] ->
+        Mix.raise """
+          Error: provide registration key
+        """
+    end
+  end
+
+end
