f33ffeb707589e771cdb138748c32c3fd5208164
Thomas Arts <thomas.arts@quviq.com>
2016-11-14 10:38:15 +0100

    Nicer pretty printing failing test cases.

--- lib/eqc/exunit.ex
+++ lib/eqc/exunit.ex
@@ -9,12 +9,8 @@ defmodule EQC.ExUnit do
     def print([term|tail]), do: pp(term) <> "\n   " <> print(tail)
 
     def pp(term) do
-      case command_sequence(term) do
-        true  ->
-          "commands"
-        false ->
-          Macro.to_string(term)
-      end
+      # Macro.to_string(term)
+      IO.iodata_to_binary(:prettypr.format(:eqc_symbolic.pretty_elixir_symbolic_doc(term), 80))
     end
 
     defp command_sequence([{:set, {:var, _}, {:call, _, _, _}}|_]), do: true
